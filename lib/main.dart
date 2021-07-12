b11d239f70d98f579d2f8dac58be86440f45f119
Anu8690 <anuragsinghal@iitg.ac.in>
2021-07-12 23:44:48 +0530

    added main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,39 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:location_tracking_app_iitg/Service/Auth_Service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:location_tracking_app_iitg/pages/Authenticate/SignInPage.dart';
+import 'package:location_tracking_app_iitg/pages/Authenticate/SignUpPage.dart';
+import 'package:location_tracking_app_iitg/pages/Groups/groups.dart';
+import 'package:location_tracking_app_iitg/pages/Groups/search.dart';
+import 'package:location_tracking_app_iitg/pages/Profile/profilePage.dart';
+import 'package:location_tracking_app_iitg/pages/wrapper.dart';
+import 'package:provider/provider.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return StreamProvider<User?>.value(
+      initialData: null,
+      value: AuthClass().user,
+      child: MaterialApp(
+        title: 'Location Tracking App',
+        initialRoute: '/',
+        routes: {
+          '/': (context) =>Wrapper(),
+          '/groups': (context) => Groups(),
+          '/search' : (context) => Search(),
+          '/login' : (context) => SignInPage(),
+          '/signup' : (context) => SignUpPage(),
+          '/profile':(context)=> ProfilePage(),
+        },
       ),
     );
   }
 }
+
